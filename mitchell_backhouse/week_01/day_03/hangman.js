cd99d72cb5b6702d8e7bb5bb8bc11c6a8b7301b8
=Mitchell Backhouse <=>
2017-07-26 16:36:21 +1000

    hangman.js - added prompt to take user input

--- mitchell_backhouse/week_01/day_03/hangman.js
+++ mitchell_backhouse/week_01/day_03/hangman.js
@@ -1,11 +1,16 @@
-// setup
+// setup the word to be used and guessed letter array
 const word = "development".split('');
 let guessedLetter = [];
 
+
 // guess a letter inside word
-const guessLetter = function (letter) {
-  for (l in word ) {
-    if (letter === word[l]) {
+const guessLetter = function () {
+
+  // get input for letter. Does not sanitise input
+  let letter = window.prompt("Enter a letter to guess");
+
+  for (chr in word ) {
+    if (letter === word[chr]) {
       if (!guessedLetter.includes(letter)) {
         guessedLetter.push(letter);
         console.log('Congratulations: Your letter was found:', letter);
@@ -13,11 +18,10 @@ const guessLetter = function (letter) {
     }
   }
   showGuess();
-  winGame();
 }
 
 // show letters guessed correctly (i.e. de_e____e__ ) and underscore all others
-const showGuess = function (param) {
+const showGuess = function (arg) {
   let arr = [];
 
   for (let i = 0; i < word.length; i++) {
@@ -25,28 +29,27 @@ const showGuess = function (param) {
       arr.push(word[i]);
     } else arr.push('_');
   }
-  if (param) {
+
+  // return only the array and don't log to console if argument is present
+  if (arg) {
     return arr;
   } else {
     console.log(arr.join(' '));
   } 
 }
 
+// check if the game has been won
 const winGame = function() {
   if (showGuess(1).join('') === word.join('')) {
     console.log("You have won the game!");
+    return true;
   }
+  return false;
 }
 
-console.log(word);
+// run the game
+while (winGame() === false) {
+  guessLetter()
+}
 
-guessLetter('e');
-guessLetter('d');
-guessLetter('x');
-guessLetter('v');
-guessLetter('l');
-guessLetter('p');
-guessLetter('m');
-guessLetter('n');
-guessLetter('t');
-guessLetter('o');
+// console.log(word);
